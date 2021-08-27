83728f47692959c43691d235ddb229aaa08caa3f
gustavorr <gustavo@constata.io>
2021-08-27 14:25:59 -0300

    connection in BD, and application get in code

--- Back/src/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ProEventos.API.Data;
 using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers
@@ -11,41 +12,22 @@ namespace ProEventos.API.Controllers
     [ApiController]
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
-    {
-        public IEnumerable<Evento> _evento = new Evento[]{
-             new Evento(){
-                    EventoId = 1,
-                    Tema = "Angular",
-                    Local = "BH",
-                    Lote ="1 lote",
-                    QtdPessoas = 250,
-                    DataEvento = DateTime.Now.ToString(),
-                    ImagemUrl = "foto.png"
-                },
-                new Evento(){
-                    EventoId = 2,
-                    Tema = "Angular 2",
-                    Local = "SP",
-                    Lote ="2 lote",
-                    QtdPessoas = 50,
-                    DataEvento = DateTime.Now.ToString(),
-                    ImagemUrl = "foto2.png"
-                }
-        };
-        public EventoController()
+    {       
+        private readonly DataContext _context;
+        public EventoController(DataContext context)
         {
-
+            _context = context;
         }
 
         [HttpGet]
         public IEnumerable<Evento> Get()
         {
-            return _evento;
+            return _context.Eventos;
         }
         [HttpGet("{id}")]
-        public IEnumerable<Evento> Get(int id)
+        public Evento Get(int id)
         {
-            return _evento.Where(evento => evento.EventoId == id);
+            return _context.Eventos.FirstOrDefault(evento => evento.EventoId == id);
         }
         [HttpPost]
         public string HttpPost()
--- Back/src/ProEventos.API/Models/Evento.cs
+++ Back/src/ProEventos.API/Models/Evento.cs
@@ -7,7 +7,6 @@ namespace ProEventos.API.Models
         public string DataEvento { get; set; }
         public string Tema { get; set; }
         public int QtdPessoas { get; set; }
-
         public string Lote { get; set; }
         public string ImagemUrl { get; set; }
     }
